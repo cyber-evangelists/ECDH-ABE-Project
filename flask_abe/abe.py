de974415e4c63b6676386cc78738d12bcfeb1d59
Talha Zafar <49996963+talhazafarjutt@users.noreply.github.com>
2023-08-04 11:39:17 +0500

    check

--- flask_abe/abe.py
+++ flask_abe/abe.py
@@ -1,5 +1,5 @@
 print('check charm..............////////////////...............')
-from flask import Flask
+from flask import Flask, request
 from charm.toolbox.pairinggroup import PairingGroup,ZR,G1,G2,GT,pair
 from charm.schemes.abenc.abenc_bsw07 import CPabe_BSW07
 app = Flask(__name__)
@@ -10,24 +10,36 @@ def hello():
         print('test api.............')
         group = PairingGroup('SS512')
         cpabe = CPabe_BSW07(group)
-        msg = 'hello from flask'
-        attributes = ['ONE', 'TWO', 'THREE']
-        access_policy = '((four or three) and (three or one))'
+
+        msg = 'Patient: John Doe, Condition: Heart Disease, Medication: ABC'
+        patient_attributes = ['DOCTOR:DrSmith', 'CONDITION:HeartDisease']
+        doctor_attributes = ['DOCTOR:DrSmith']
+
+        access_policy = '((DOCTOR:DrSmith and CONDITION:HeartDisease) or DOCTOR:DrBrown)'
+
         (master_public_key, master_key) = cpabe.setup()
-        secret_key = cpabe.keygen(master_public_key, master_key, attributes)
+
+        patient_secret_key = cpabe.keygen(master_public_key, master_key, patient_attributes)
+
         cipher_text = cpabe.encrypt(master_public_key, msg, access_policy)
-        decrypted_msg = cpabe.decrypt(master_public_key, secret_key, cipher_text)
-        print('decrypted_msg is:',decrypted_msg)
-        return 'sdfghjk'
+
+        doctor_secret_key = cpabe.keygen(master_public_key, master_key, doctor_attributes)
+        decrypted_msg = cpabe.decrypt(master_public_key, doctor_secret_key, cipher_text)
+
+        print("Decrypted Message by Doctor:", decrypted_msg)
+
+        return 'sdfghj14526k'
     except Exception as error:
         print('error is:',error)
 
-@app.route("/encryption")
+@app.route("/encryption", methods=['POST'])
 def encryption():
     try:
-        pass
+        input_json = request.get_json(force=True) 
+        group = PairingGroup('SS512')
+        cpabe = CPabe_BSW07(group)
+        return input_json
     except:
         pass
 if __name__ == "__main__":
-    print('main flask.............')
     app.run(host='172.29.0.16', port=5001)
